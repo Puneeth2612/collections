44565dbd4903cb8859f7b51426bcf31bd58f0767
Puneeth2612 <31363428+Puneeth2612@users.noreply.github.com>
2019-02-13 22:39:45 +0530

    Proper Namin

--- CollectionsFramework/src/com/sorting/examples/comparable/SortingComparable.java
+++ CollectionsFramework/src/com/sorting/examples/comparable/SortingComparable.java
@@ -3,19 +3,31 @@ package com.sorting.examples.comparable;
 import java.util.ArrayList;
 import java.util.*;
 
+/**
+ * 
+ * This class Contains example comparable 
+ *
+ */
 public class SortingComparable {
 
 	public static void main(String[] args) {
 		List<Student> st =new ArrayList<>();
-		st.add(new Student("Puneeth2",5));
-		st.add(new Student("Puneeth",1  ));
-		st.add(new Student("Puneeth2",2));
+		st.add(new Student("Student5",5));
+		st.add(new Student("Student1",1  ));
+		st.add(new Student("Student2",2));
 		Collections.sort(st);
 		
-		List list=new ArrayList<String>();
-		list.add("Puneeth");
-		list.add("Jayanth");
-		list.add("Dude");
+		List<String> list=new ArrayList<String>();
+		list.add("test4");
+		list.add("test2");
+		list.add("test3");
+		
+		Collections.sort(list);
+		
+		for (String stdnt : list) {
+			System.out.println(stdnt);
+		}
+		System.out.println(list);
 		
 		System.out.println(st);
 		
